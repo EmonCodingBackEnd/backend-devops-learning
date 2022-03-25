1fd3ba659ad2b0ac8e8c16dbee30585c89ed0cf5
emon <liming20110711@163.com>
2022-03-25 16:17:36 +0800

    update:20220325-08

--- user-thrift-service/build.sh
+++ user-thrift-service/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 用例： [emon@emon user-thrift-service]$ sudo sh build.sh
+# 用例： [emon@emon backend-devops-learning]$ sh user-thrift-service/build.sh
 
 cd /home/emon/dockerdata/backend-devops-learning
 mvn clean package -DskipTests
@@ -8,4 +8,4 @@ mvn clean package -DskipTests
 sleep 2
 
 cd /home/emon/dockerdata/backend-devops-learning/user-thrift-service
-docker build -t user-thrift-service:latest .
+sudo docker build -t user-thrift-service:latest .
