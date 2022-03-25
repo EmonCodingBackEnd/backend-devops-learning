de6ba26794692712c929e317d5e3b483272ed4cc
emon <liming20110711@163.com>
2022-03-26 07:58:29 +0800

    update:20220326-01

--- course-edge-service/build.sh
+++ course-edge-service/build.sh
@@ -8,4 +8,4 @@ mvn clean package -DskipTests
 cd /home/emon/dockerdata/backend-devops-learning/course-edge-service
 sudo docker build -t course-edge-service:latest .
 
-# 启动容器： [emon@emon backend-devops-learning]$ docker run -it course-edge-service:latest --zookeeper.address=10.0.0.116
+# 启动容器： [emon@emon backend-devops-learning]$ docker run -it course-edge-service:latest --zookeeper.address=172.17.0.1
--- user-edge-service/build.sh
+++ user-edge-service/build.sh
@@ -8,4 +8,4 @@ mvn clean package -DskipTests
 cd /home/emon/dockerdata/backend-devops-learning/user-edge-service
 sudo docker build -t user-edge-service:latest .
 
-# 启动容器： [emon@emon backend-devops-learning]$ docker run -it user-edge-service:latest --redis.address=10.0.0.116
+# 启动容器： [emon@emon backend-devops-learning]$ docker run -it user-edge-service:latest --redis.address=172.17.0.1
