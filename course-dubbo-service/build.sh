6176ad92d8a78b708ba8578d006e5fcc34139c18
emon <liming20110711@163.com>
2022-03-25 23:11:40 +0800

    update:20220325-25

--- course-dubbo-service/build.sh
+++ course-dubbo-service/build.sh
@@ -8,4 +8,4 @@ mvn clean package -DskipTests
 cd /home/emon/dockerdata/backend-devops-learning/course-dubbo-service
 sudo docker build -t course-service:latest .
 
-# 启动容器： [emon@emon backend-devops-learning]$ docker run -it course-service:latest --mysql.address=10.0.0.116 --zookeeper.address=10.0.0.116
+# 启动容器： [emon@emon backend-devops-learning]$ docker run -it course-service:latest --mysql.address=192.168.1.116 --zookeeper.address=192.168.1.116
