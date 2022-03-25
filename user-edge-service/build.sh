fdb1300d96c268b0c7685974bcf8acf6a0df20b7
emon <liming20110711@163.com>
2022-03-25 17:08:20 +0800

    update:20220325-17

---
+++ user-edge-service/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 用例： [emon@emon backend-devops-learning]$ sh user-edge-service/build.sh
+
+cd /home/emon/dockerdata/backend-devops-learning
+mvn clean package -DskipTests
+
+cd /home/emon/dockerdata/backend-devops-learning/user-edge-service
+sudo docker build -t user-edge-service:latest .
+
+# 启动容器： [emon@emon backend-devops-learning]$ docker run -it user-edge-service:latest -redis.address=10.0.0.116
