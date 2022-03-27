bb99cc842ab2426ff2b866eda469dff050334fa7
emon <liming20110711@163.com>
2022-03-27 23:28:25 +0800

    update:20220327-03

--- api-gateway-zuul/build.sh
+++ api-gateway-zuul/build.sh
@@ -8,5 +8,5 @@ mvn clean package -DskipTests
 cd /home/emon/dockerdata/backend-devops-learning/api-gateway-zuul
 sudo docker build -t emon:5080/devops-learning/api-gateway:latest .
 
-docker push emon:5080/devops-learning/api-gateway:latest
+sudo docker push emon:5080/devops-learning/api-gateway:latest
 # 启动容器： [emon@emon backend-devops-learning]$ docker run -it api-gateway:latest
