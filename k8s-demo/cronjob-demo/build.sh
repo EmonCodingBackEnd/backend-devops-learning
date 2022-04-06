311c99a0a80f9d74e882dd01f50e9a4a412b03ba
emon <liming20110711@163.com>
2022-04-06 22:50:19 +0800

    update:20220406-02

--- k8s-demo/cronjob-demo/build.sh
+++ k8s-demo/cronjob-demo/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# 用例： [emon@emon backend-devops-learning]$ sh k8s-demo/cronjob-demo/build.sh
-
 cd /root/dockerdata/backend-devops-learning
 mvn clean package -DskipTests
 
@@ -10,4 +8,6 @@ docker build -t 192.168.200.116:5080/devops-learning/cronjob-demo:latest .
 
 docker push 192.168.200.116:5080/devops-learning/cronjob-demo:latest
 
-# 启动容器： [emon@emon backend-devops-learning]$ docker run -it 192.168.200.116:5080/devops-learning/cronjob-demo:latest
+# 执行目录： $ cd /root/dockerdata/backend-devops-learning
+# 执行构建： $ sh k8s-demo/cronjob-demo/build.sh
+# 启动容器： $ docker run -it 192.168.200.116:5080/devops-learning/cronjob-demo:latest
