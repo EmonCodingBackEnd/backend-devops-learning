ef856be464ec6cbf6b43415b764541da55354480
emon <liming20110711@163.com>
2022-04-07 18:03:50 +0800

    update:20220407-05

--- k8s-demo/springboot-web-demo/build.sh
+++ k8s-demo/springboot-web-demo/build.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-cd /root/dockerdata/backend-devops-learning
-mvn clean package -DskipTests
+pwd
+mvn mvn -pl k8s-demo/springboot-web-demo/ -am clean package -Dmaven.test.skip=true
 
-cd /root/dockerdata/backend-devops-learning/k8s-demo/springboot-web-demo
+pwd
+cd k8s-demo/springboot-web-demo
+pwd
+
 docker build -t 192.168.200.116:5080/devops-learning/springboot-web-demo:latest .
+cd "$WORKSPACE"
+pwd
 
-docker push 192.168.200.116:5080/devops-learning/springboot-web-demo:latest
+#docker push 192.168.200.116:5080/devops-learning/springboot-web-demo:latest
 
 # 执行目录： $ cd /root/dockerdata/backend-devops-learning
 # 执行构建： $ sh k8s-demo/springboot-web-demo/build.sh
