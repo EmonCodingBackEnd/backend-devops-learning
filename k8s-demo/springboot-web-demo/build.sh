3882bd4c7a4f1bd472aebac832c2b12a31ea8dff
emon <liming20110711@163.com>
2022-04-10 15:51:14 +0800

    update:20220409-06

--- k8s-demo/springboot-web-demo/build.sh
+++ k8s-demo/springboot-web-demo/build.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+version=latest
+if [ $# -gt 0 ]; then
+  version=$1
+fi
+
 cd /root/dockerdata/backend-devops-learning
 #mvn clean package -DskipTests
 mvn -pl k8s-demo/springboot-web-demo -am clean package -Dmaven.test.skip=true
 
 cd /root/dockerdata/backend-devops-learning/k8s-demo/springboot-web-demo
-docker build -t 192.168.200.116:5080/devops-learning/springboot-web-demo:latest .
+docker build -t 192.168.200.116:5080/devops-learning/springboot-web-demo:"${version}" .
 
-docker push 192.168.200.116:5080/devops-learning/springboot-web-demo:latest
+docker push 192.168.200.116:5080/devops-learning/springboot-web-demo:"${version}"
 
 # 执行目录： $ cd /root/dockerdata/backend-devops-learning
 # 执行构建： $ sh k8s-demo/springboot-web-demo/build.sh
