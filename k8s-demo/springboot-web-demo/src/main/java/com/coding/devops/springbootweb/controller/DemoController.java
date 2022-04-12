f97744e3cd23e6cb2d9bbba69e20462120105418
emon <liming20110711@163.com>
2022-04-12 09:07:26 +0800

    update:20220412-01

--- k8s-demo/springboot-web-demo/src/main/java/com/coding/devops/springbootweb/controller/DemoController.java
+++ k8s-demo/springboot-web-demo/src/main/java/com/coding/devops/springbootweb/controller/DemoController.java
@@ -1,15 +1,19 @@
 package com.coding.devops.springbootweb.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
+    private static final Logger log = LoggerFactory.getLogger(DemoController.class);
 
     @RequestMapping("/hello")
     public String sayHello(@RequestParam String name) {
-
-        return "Hello " + name + "! I'm springboot-web-demo controller!bluegreen-v1.0";
+        String msg = "Hello " + name + "! I'm springboot-web-demo controller!";
+        log.info(msg);
+        return msg;
     }
 }
